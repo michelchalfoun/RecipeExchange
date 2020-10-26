38d921c379fb995a76569c3a081534d7c07f4a9e
Michel Chalfoun <michelchalfoun@Michels-MacBook-Pro.local>
2020-10-26 17:57:19 -0400

    Cleaned Header

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import AuthenticationButton from '../Auth/AuthenticationButton';
 import './Header.css';
 
 const Header = () => {
@@ -12,17 +13,8 @@ const Header = () => {
 
             {/* Page Links */}
             <div className="topnav-right">
-                <Link className="topnav-link" to='/Register'>Sign in</Link>
-                <Link className="topnav-link" to='/projects'>Projects</Link>
-                <a className="topnav-link" target='_blank' rel="noopener noreferrer" href="https://www.facebook.com/groups/ufosc/events/?source=4&action_history=null&filter=calendar">
-                    Events
-                    <i className="fas fa-external-link-alt external-link" data-fa-transform="up-6"></i>
-                </a>
-                <a className="topnav-link" target='_blank' rel="noopener noreferrer" href="https://github.com/ufosc/club-resources">
-                    Resources
-                    <i className="fas fa-external-link-alt external-link" data-fa-transform="up-6 right-4"></i>
-                </a>
-                <Link className="topnav-link" to="/about">About</Link>
+                <Link className="topnav-link" to='/Home'>Home</Link>
+                <AuthenticationButton></AuthenticationButton>
             </div>
         </div>
     )
