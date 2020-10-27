38f2c648ce3bb16da776694e1e09c495360a3373
Michel Chalfoun <michelchalfoun@Michels-MacBook-Pro.local>
2020-10-27 11:41:56 -0400

    Formatted the Login/Logout links

--- client/src/components/Auth/LogoutButton.js
+++ client/src/components/Auth/LogoutButton.js
@@ -1,15 +1,11 @@
 import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
-import { Button } from 'react-bootstrap'
+import { Nav } from 'react-bootstrap'
 
 const LogoutButton = () => {
   const { logout } = useAuth0();
 
-  return (
-    <Button onClick={() => logout({ returnTo: window.location.origin })}>
-      Log Out
-    </Button>
-  );
+  return <Nav.Link onClick={() => logout({ returnTo: window.location.origin })}>Log Out</Nav.Link>
 };
 
 export default LogoutButton;
