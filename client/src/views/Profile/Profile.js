012ab36ca8de6c487592373f0aea1d9dec682692
Michel Chalfoun <michelchalfoun@Michels-MacBook-Pro.local>
2020-10-27 12:01:38 -0400

    Reformated Profile Page

--- client/src/views/Profile/Profile.js
+++ client/src/views/Profile/Profile.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { useAuth0 } from "@auth0/auth0-react";
 import { Jumbotron, Button } from 'react-bootstrap'
 
 function Profile() {
@@ -7,12 +6,8 @@ function Profile() {
         <div >
             <Jumbotron>
                 <h1>Hello, user!</h1>
-                <p>
-                    You are currently logged in and this is your profile page.
-  </p>
-                <p>
-                    <Button href="/Home" variant="primary">Go back home</Button>
-                </p>
+                <p>You are currently logged in and this is your profile page.</p>
+                <Button href="/Home" variant="primary">Go back home</Button>
             </Jumbotron>
         </div>
     );
