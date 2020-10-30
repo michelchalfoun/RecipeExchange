0c663935cc265433208ad5249ffad61a47b78bf0
Dallas Tealer <dallas@Dallass-MacBook-Pro.local>
2020-10-30 01:58:04 -0400

    Added database to server.js

--- server/server.js
+++ server/server.js
@@ -1,4 +1,15 @@
 const express = require('./config/express.js')
+const mongoose = require('mongoose');
+
+
+//Database setup (Dallas)
+const uri = "mongodb+srv://dallas:Elisha0924@cluster0.xaiwm.mongodb.net/RecipeExchange?retryWrites=true&w=majority"
+mongoose.connect(uri, {useNewUrlParser: true, useCreateIndex: true});
+
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log("MongoDB database connection established successfully");
+})
  
 // Use env port or default
 const port = process.env.PORT || 5000;
