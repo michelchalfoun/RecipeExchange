c1f096501088be816993b0074f01a4ff8d54bc27
Michel Chalfoun <michelchalfoun@Michels-MacBook-Pro.local>
2020-11-06 11:39:27 -0500

    Fixed request bug for donate page

--- client/src/components/GoogleMap/GoogleMap.js
+++ client/src/components/GoogleMap/GoogleMap.js
@@ -2,7 +2,39 @@ import React, { Component } from 'react';
 import GoogleMapReact from 'google-map-react';
 import axios from 'axios';
 
+
 class SimpleMap extends Component {
+
+    constructor(props) {
+        super(props)
+
+        //this.getCharities = this.getCharities.bind(this);
+
+        this.state = {
+            isLoading: false,
+            charities: [],
+        }
+    }
+
+    componentDidMount() {
+        this.setState({ isLoading: true })
+        const params = {
+            key: 'AIzaSyDOH-pA2icLq8Lcwmqr0gqtUDCvRkc3LVw',
+            location: '29.6345,-82.3371',
+            radius: '5000',
+            keyword: 'charities'
+        }
+        const headers = {
+            'Access-Control-Allow-Origin': '*'
+        }
+        axios.get('https://maps.googleapis.com/maps/api/place/nearbysearch/json', { params: params }, { headers: headers })
+            .then((res) => {
+                this.state.charities = res.data.results
+                this.setState({ isLoading: false })
+            }).catch((error) => {
+                console.log(error)
+            });
+    }
 
     static defaultProps = {
         center: {
@@ -13,32 +45,20 @@ class SimpleMap extends Component {
     };
 
     render() {
-        const params = {
-            key: 'AIzaSyDOH-pA2icLq8Lcwmqr0gqtUDCvRkc3LVw',
-            location: '29.6345,-82.3371',
-            radius: '5000',
-            keyword: 'charities'
-        }
-        const headers = {
-            "Access-Control-Allow-Origin": "*"
-        }
-        axios.get('https://maps.googleapis.com/maps/api/place/nearbysearch/json?', 
-        {params: params}, {headers: headers})
-            .then((res) => {
-                const charities = res.data
-            }).catch((error) => {
-                console.log(error)
-            });
-        console.log(charities)
         return (
             <div style={{ height: '100vh', width: '100%' }}>
-                <GoogleMapReact
-                    bootstrapURLKeys={{ key: 'AIzaSyDOH-pA2icLq8Lcwmqr0gqtUDCvRkc3LVw' }}
-                    defaultCenter={this.props.center}
-                    defaultZoom={this.props.zoom}
-                >
-                    {charities.results.map(item => <MapMarker key={item.place_id} place_id={item.place_id} lat={item.geometry.location.lat} lng={item.geometry.location.lng} text={item.name} />)}
-                </GoogleMapReact>
+                {!this.state.isLoading && (
+                    <GoogleMapReact
+                        bootstrapURLKeys={{ key: 'AIzaSyDOH-pA2icLq8Lcwmqr0gqtUDCvRkc3LVw' }}
+                        defaultCenter={this.props.center}
+                        defaultZoom={this.props.zoom}
+                    >
+                        {this.state.charities.map(item => <MapMarker key={item.place_id} place_id={item.place_id} lat={item.geometry.location.lat} lng={item.geometry.location.lng} text={item.name} />)}
+                    </GoogleMapReact>
+                )}
+                {this.state.isLoading && (
+                    <h1>Loading</h1>
+                )}
             </div>
         );
     }
@@ -56,8 +76,6 @@ class MapMarker extends React.Component {
         return (
             <div style={{ cursor: "pointer" }} key={this.props.key}>
                 <i onClick={openCharity} class="building icon" size="large"></i>
-                {/* {this.props.text} */}
-
             </div>
         )
     }
