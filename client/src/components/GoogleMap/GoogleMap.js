3b233ebfb7e55e946a0f7e0e715fe8ae5fb8cb33
Michel Chalfoun <michelchalfoun@michels-mbp.lan>
2020-11-08 02:08:02 -0500

    Implemented curent location and loading page

--- client/src/components/GoogleMap/GoogleMap.js
+++ client/src/components/GoogleMap/GoogleMap.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import GoogleMapReact from 'google-map-react';
+import { Spinner } from 'react-bootstrap'
 import axios from 'axios';
 
 class SimpleMap extends Component {
@@ -9,43 +10,69 @@ class SimpleMap extends Component {
         this.state = {
             isLoading: true,
             charities: [],
+            coordinates: {
+                lat: 0,
+                lng: 0
+            }
         }
     }
 
     componentDidMount() {
         this.setState({ isLoading: true })
-        console.log('LMAO')
-        axios.get('http://localhost:5000/charity/getCharities')
-            .then((res) => {
-                this.setState({ charities: res.data })
-                this.setState({ isLoading: false })
-            }).catch((error) => {
-                console.log(error)
-            });
+
+        navigator.geolocation.getCurrentPosition(function (position) {
+            this.setState({
+                coordinates: {
+                    lat: position.coords.latitude,
+                    lng: position.coords.longitude
+                }
+            })
+            axios.get('http://localhost:5000/charity/getCharities',
+                {
+                    params: {
+                        coordinates: String(position.coords.latitude) + ',' + String(position.coords.longitude)
+                    }
+                })
+                .then((res) => {
+                    this.setState({
+                        charities: res.data,
+                        isLoading: false
+                    })
+                }).catch((error) => {
+                    console.log(error)
+                });
+
+        }.bind(this))
+
+
     }
 
-    static defaultProps = {
-        center: {
-            lat: 29.6345,
-            lng: -82.3371
-        },
-        zoom: 15
-    };
+
 
     render() {
+        console.log('render')
+        console.log(this.state.coordinates)
         return (
             <div style={{ height: '100vh', width: '100%' }}>
                 {!this.state.isLoading && (
                     <GoogleMapReact
                         bootstrapURLKeys={{ key: 'AIzaSyDOH-pA2icLq8Lcwmqr0gqtUDCvRkc3LVw' }}
-                        defaultCenter={this.props.center}
-                        defaultZoom={this.props.zoom}
+                        defaultCenter={this.state.coordinates}
+                        defaultZoom={15}
                     >
                         {this.state.charities.map(item => <MapMarker key={item.place_id} place_id={item.place_id} lat={item.geometry.location.lat} lng={item.geometry.location.lng} text={item.name} />)}
                     </GoogleMapReact>
                 )}
                 {this.state.isLoading && (
-                    <h1>Loading</h1>
+                    <div style={{
+                        display: "flex",
+                        justifyContent: "center",
+                        alignItems: "center"
+                    }}>
+                        <Spinner animation="border" role="status" >
+                            <span className="sr-only">Loading...</span>
+                        </Spinner>
+                    </div>
                 )}
             </div>
         );
--- server/routes/charity.routes.js
+++ server/routes/charity.routes.js
@@ -6,10 +6,11 @@ const router = express.Router();
 router.route('/getCharities').get((request, response) => {
     const params = {
         key: 'AIzaSyDOH-pA2icLq8Lcwmqr0gqtUDCvRkc3LVw',
-        location: '29.6345,-82.3371',
+        location: request.query.coordinates,
         radius: '5000',
-        keyword: 'charities'
+        keyword: 'charity'
     }
+    console.log(request)
     axios.get('https://maps.googleapis.com/maps/api/place/nearbysearch/json', { params: params })
             .then((res) => {
                 response.send(res.data.results)
