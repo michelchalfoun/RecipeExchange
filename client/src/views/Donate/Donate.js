0d685ffc55750ee97c34a566724046224622217d
Michel Chalfoun <michelchalfoun@michels-mbp.lan>
2020-11-08 02:09:36 -0500

    Typo

--- client/src/views/Donate/Donate.js
+++ client/src/views/Donate/Donate.js
@@ -8,7 +8,7 @@ function Donate() {
             <Jumbotron>
                 <h1>Have some leftover ingredients?.</h1>
                 <p>Check out the map below to find nearby charities to donate to.</p>
-                <p>Clikc on the building icons to find out more information about each charity.</p>
+                <p>Click on the building icons to find out more information about each charity.</p>
             </Jumbotron>
             <SimpleMap></SimpleMap>
         </div>
