5e2837430a5a744010666c6df2ba576e030c51d5
Michel Chalfoun <michelchalfoun@Michels-MacBook-Pro.local>
2020-12-09 16:06:53 -0500

    Removed whitespace

--- server/config/config.js
+++ server/config/config.js
@@ -1,6 +1,3 @@
-//This file holds any configuration variables we may need
-//'config.js' is usually ignored by git to protect sensitive information, such as your database's username and password
-
 module.exports = {
     db: {
         uri: 'mongodb+srv://dbUser:dbUserPassword@cluster0.qri3r.mongodb.net/RecipeExchangeDB?retryWrites=true&w=majority',
